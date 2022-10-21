91b11f92fa389f4f71583b9abdacf85573a46997
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-21 15:00:42 +0200

    dodane zmienne

--- app/src/main/java/com/example/apka2110/MainActivity.kt
+++ app/src/main/java/com/example/apka2110/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.apka2110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dopisz = findViewById<Button>(R.id.button_dop)
+        val przywroc = findViewById<Button>(R.id.button_pr)
+        val tekst = findViewById<TextView>(R.id.textView)
+        val wpisz = findViewById<EditText>(R.id.editTextTextPersonName)
+
     }
+
 }
